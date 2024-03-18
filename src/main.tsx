0bcfab3b4d2b973dd01b56cbbffb5df40f8e89b7
André Hammarstig <andre.hammarstig@ahlsell.se>
2024-03-18 14:37:53 +0100

    prettify

--- src/main.tsx
+++ src/main.tsx
@@ -79,5 +79,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
